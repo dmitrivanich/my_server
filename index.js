b8b91cdd95b8ff713da619e297d1c06cfc35e2bd
Dmitriy T <80646636+dmitrivanich@users.noreply.github.com>
2023-04-28 17:52:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-// Разрешаем доступ с источника http://localhost:5173
+// Разрешаем доступ с источника *
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
